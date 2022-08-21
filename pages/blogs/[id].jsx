8d7634a79e17cd40f0d0c79083bdb23abaacc638
IrlanFardinata <fardinatairlan@gmail.com>
2022-08-22 02:03:41 +0700

    refaktor fungsi tambah edit hapus blog

--- pages/blogs/[id].jsx
+++ pages/blogs/[id].jsx
@@ -21,37 +21,37 @@ const BlogDetail = ({data}) =>{
             <Layouts>
                     <section className="bg-white border-b py-8 mt-16">
                         <div className="container mx-auto flex flex-wrap pt-4 pb-12">
-                        <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-                            Title
-                        </h2>
-                        <div className="w-full mb-4">
-                            <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t" />
-                        </div>
+                            <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+                                Title
+                            </h2>
+                            <div className="w-full mb-4">
+                                <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t" />
+                            </div>
 
-                        <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-                            <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-                            <a href="#" className="flex flex-wrap no-underline hover:no-underline">
-                                <p className="w-full text-gray-600 text-xs md:text-sm px-6">
-                                {data.id}
-                                </p>
-                                <div className="w-full font-bold text-xl text-gray-800 px-6">
-                                {data.title}
+                            <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                                <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+                                    <a href="#" className="flex flex-wrap no-underline hover:no-underline">
+                                        <p className="w-full text-gray-600 text-xs md:text-sm px-6">
+                                        {data.id}
+                                        </p>
+                                        <div className="w-full font-bold text-xl text-gray-800 px-6">
+                                        {data.title}
+                                        </div>
+                                        <p className="text-gray-800 text-base px-6 mb-5">
+                                        {data.body}
+                                        </p>
+                                    </a>
                                 </div>
-                                <p className="text-gray-800 text-base px-6 mb-5">
-                                 {data.body}
-                                </p>
-                            </a>
+                                <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+                                    <div className="flex items-center justify-center">
+                                        <Link href={`/blogs`}>
+                                            <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                                                Back
+                                            </button>
+                                        </Link>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                            <div className="flex items-center justify-center">
-                                <Link href={`/blogs`}>
-                                <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                                    Back
-                                </button>
-                                </Link>
-                            </div>
-                            </div>
-                        </div>
                         </div>
                     </section>
                      <svg className="wave-top" viewBox="0 0 1439 147" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">

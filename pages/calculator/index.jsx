24307391e4155f3346663c6f8ce7e1b295644ef4
IrlanFardinata <fardinatairlan@gmail.com>
2022-08-30 10:36:53 +0700

    value input

--- pages/calculator/index.jsx
+++ pages/calculator/index.jsx
@@ -48,14 +48,14 @@ const Calculator = () =>{
                                                 <div className="mb-3">
                                                     <input type="number" className="form-control block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding
                                                         border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                                    id="input1" placeholder="Input Number 1" value={0}/>
+                                                    id="input1" placeholder="Input Number 1"/>
                                                 </div>
                                             </div>
                                             <div className="flex justify-center">
                                                 <div className="mb-3">
                                                     <input type="number" className="form-control block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding
                                                         border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                                    id="input2" placeholder="Input Number 2" value={0}/>
+                                                    id="input2" placeholder="Input Number 2"/>
                                                 </div>
                                             </div>
                                         </div>
